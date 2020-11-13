dda16b39048dc17ebafcebcea7f47a7764bb444c
shervin <en.severus@gmail.com>
2020-11-13 21:20:33 +0330

    pictures...

---
+++ pic/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>ERROR 404</title>
+    </head>
+    <body>
+        <h1>ERROR 404</h1>
+        <br>
+        <h3>Page Not Found!</h3>
+    </body>
+</html>
