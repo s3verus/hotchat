191347f933cdf336f930e92f522c7012d74702c1
shervin <en.severus@gmail.com>
2020-11-13 21:26:59 +0330

    page for sending messages...

---
+++ chat.php
@@ -0,0 +1,35 @@
+<div class="chat-main">
+    <div class="chat-bar">
+        <div class="info">
+                <img src="pic/default.png" alt="user picture"> <!-- solve it -->
+        </div>
+    </div>
+    <div class="chat-body">
+        <?php
+            $user=$_SESSION["username"];
+            $res=getChats($user);
+            while($row=mysqli_fetch_array($res,1)):
+        ?>
+                <div class="messages">
+                    <div class="message-pic">
+                        <img src="pic/default.png" alt="user picture">
+                    </div>
+                    <div class="pointer">
+
+                    </div>
+                    <div class="message-text">
+                        <h1><?php echo $row["message"]; ?></h1>
+                    </div>
+                </div>
+        <?php
+            $i=$i+1;
+            endwhile;
+        ?>
+        <!-- TODO solve sending messages -->
+    </div>
+    <div class="chat-line">
+        <a href="#"><i class="fas fa-paperclip"></i></a>
+        <textarea class="message-bar" name="message" placeholder="write somethings..."></textarea>
+        <input class="chat-btm" type="submit" name="send" value="send">
+    </div>
+</div>
